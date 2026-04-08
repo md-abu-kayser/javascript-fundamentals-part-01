ef2a9c22e31f1c5b498b92b907630bee75fa9b8b
md-abu-kayser <abu.kayser.official@gmail.com>
2026-04-08 21:01:36 +0600

    feat(conditional): demonstrate ternary operator including nested conditions

--- js-fundamental-conditionals/ternary.js
+++ js-fundamental-conditionals/ternary.js
@@ -1,10 +1,10 @@
 /**
  * TERNARY --> three parts
- * 
+ *
  *   ?    :
- * 
+ *
  *  condition ? do something when true : do something when false
-*/
+ */
 
 const age = 112;
 
@@ -17,40 +17,34 @@ const age = 112;
 // }
 
 // simple ternary
-age >= 18 ? console.log('Vote Dio') : console.log('Ghumai thako')
+age >= 18 ? console.log("Vote Dio") : console.log("Ghumai thako");
 
 let price = 500;
 const isLeader = false;
 
-if(isLeader === true){
-    price = 0;
-}
-else {
-    price = price + 100;
+if (isLeader === true) {
+  price = 0;
+} else {
+  price = price + 100;
 }
 // console.log(price)
 
 price = isLeader === true ? 0 : price + 100;
 
-
 // OPTIONAL: semi-advanced ternary
 
-if(isLeader === true){
-    if(price > 1000) {
-        price = price /2;
-    }
-    else {
-        price = 0;
-    }
+if (isLeader === true) {
+  if (price > 1000) {
+    price = price / 2;
+  } else {
+    price = 0;
+  }
+} else {
+  price = price + 1000;
 }
-else {
-    price = price + 1000;
-}
-
 
 // Feel free to ignore this one
 // price = isLeader === true ? price > 1000 ? price /2 : 0 : price + 1000;
-price = isLeader === true ? 
-            price > 1000 ? 
-                price /2 : 0 
-        : price + 1000;
+price = isLeader === true ? (price > 1000 ? price / 2 : 0) : price + 1000;
+
+//
